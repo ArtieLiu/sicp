35079ed2228c1c73f345f56a2afd56cf420618b3
ArtieLiu <yinan97@outlook.com>
2022-11-01 23:22:29 +0800

    add solution of e3.32

---
+++ chapter3/3.3.4/e3.32.scm
@@ -0,0 +1,4 @@
+; The two inputs of and gate cannot be set at the same time, 
+; they must be set one by one. 
+; The actions saved in queue should be executed by the order they occur and this is gaureented by using FIFO queue. 
+; If a LIFO queue is used, the actions in the siumulation would be executed in reverse time.
